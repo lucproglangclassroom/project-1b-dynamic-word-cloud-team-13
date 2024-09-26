ae0e693d953b3f83454cbcc4b9a903e7aea84998
Ella McKenney <ellavmckenney@gmail.com>
2024-09-26 16:24:47 +0000

    Update functional scala code

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -1,39 +1,55 @@
 package hellotest
 
-import scala.collection.mutable.{Map, Queue}
+import scala.io.StdIn
+import scala.collection.mutable
 import scala.language.unsafeNulls
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    val cloudSize = args.headOption.map(_.toInt).getOrElse(10)
-    val minLength = args.lift(1).map(_.toInt).getOrElse(1)
-    val windowSize = args.lift(2).map(_.toInt).getOrElse(100)
-    val wordCounts = Map.empty[String, Int] 
-    val recentWords = Queue.empty[String]
+object TopWords {
 
-    def updateWordCloud() = {
-      val sortedCounts = wordCounts.toSeq.sortBy(-_._2).take(cloudSize)
-      println(sortedCounts.map { case (word, count) => s"$word: $count" }.mkString(" "))
+  // Helper method to parse command line arguments
+  def parseArgs(args: Array[String]): (Int, Int, Int) = {
+    val argMap = args.sliding(2, 2).collect {
+      case Array("-c", size) => "cloudSize" -> size.toInt
+      case Array("-l", length) => "minLength" -> length.toInt
+      case Array("-w", size) => "windowSize" -> size.toInt
+    }.toMap
+
+    val cloudSize = argMap.getOrElse("cloudSize", 10)
+    val minLength = argMap.getOrElse("minLength", 6)
+    val windowSize = argMap.getOrElse("windowSize", 1000)
+    (cloudSize, minLength, windowSize)
+  }
+
+  // Method to update and print the word cloud
+  def printWordCloud(words: mutable.Queue[String], cloudSize: Int): Unit = {
+    val wordCounts = words.groupBy(identity).view.mapValues(_.size).toSeq.sortBy(-_._2).take(cloudSize)
+    println(wordCounts.map { case (word, count) => s"$word: $count" }.mkString(" "))
+
+    // to handle SIGPIPE
+    if (System.out.checkError()) {
+      System.exit(1)
     }
+  }
 
-    for (line <- scala.io.Source.stdin.getLines()) {
-      line.split("\\s+").filter(_ != null).filter(_.nonEmpty).foreach { word =>
-        if (word.length >= minLength) {
-          val lowerCaseWord = word.toLowerCase
-          wordCounts.update(lowerCaseWord, wordCounts.getOrElse(lowerCaseWord, 0) + 1)
-          recentWords.enqueue(lowerCaseWord)
-          if (recentWords.size > windowSize) {
-            val removedWord = recentWords.dequeue()
-            wordCounts.update(removedWord, wordCounts(removedWord) - 1)
-          }
+  def main(args: Array[String]): Unit = {
+    val (cloudSize, minLength, windowSize) = parseArgs(args)
+    val words = mutable.Queue[String]()
+
+    while (true) {
+      val input = StdIn.readLine()
+      if (input == null) sys.exit(0)
+      val validWords = input.split("\\s+").filter(_.length >= minLength)
+
+      words.enqueueAll(validWords)
+      if (words.length > windowSize) {
+        val excess = words.length - windowSize
+        for (_ <- 1 to excess) words.dequeue()
         }
-      }
-      //to handle SIGPIPE
-      if (System.out.checkError()) {
-        System.exit(1)
-      }
 
-      updateWordCloud()
+
+      if (words.nonEmpty) {
+        printWordCloud(words, cloudSize)
+        }
     }
   }
 }
