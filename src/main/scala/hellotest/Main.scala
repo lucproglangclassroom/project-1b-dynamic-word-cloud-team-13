701ca1fe8356bcdf7e99c939caece3ec8872736a
Ella McKenney <ellavmckenney@gmail.com>
2024-09-29 22:34:44 +0000

    Make updates to main

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -2,54 +2,83 @@ package hellotest
 
 import scala.io.StdIn
 import scala.collection.mutable
+import java.io.PrintStream
 import scala.language.unsafeNulls
+
+case class Config(cloudSize: Int = 10, minLength: Int = 6, windowSize: Int = 1000)
 
 object TopWords {
 
-  // Helper method to parse command line arguments
-  def parseArgs(args: Array[String]): (Int, Int, Int) = {
-    val argMap = args.sliding(2, 2).collect {
-      case Array("-c", size) => "cloudSize" -> size.toInt
-      case Array("-l", length) => "minLength" -> length.toInt
-      case Array("-w", size) => "windowSize" -> size.toInt
-    }.toMap
-
-    val cloudSize = argMap.getOrElse("cloudSize", 10)
-    val minLength = argMap.getOrElse("minLength", 6)
-    val windowSize = argMap.getOrElse("windowSize", 1000)
-    (cloudSize, minLength, windowSize)
+  def parseArgs(args: Array[String]): Config = {
+    args.sliding(2, 2).foldLeft(Config()) { (config, argPair) =>
+      argPair match {
+        case Array("-c", size)    => config.copy(cloudSize = size.toInt)
+        case Array("-l", length)  => config.copy(minLength = length.toInt)
+        case Array("-w", size)    => config.copy(windowSize = size.toInt)
+        case _                    => config
+      }
+    }
   }
 
-  // Method to update and print the word cloud
-  def printWordCloud(words: mutable.Queue[String], cloudSize: Int): Unit = {
-    val wordCounts = words.groupBy(identity).view.mapValues(_.size).toSeq.sortBy(-_._2).take(cloudSize)
-    println(wordCounts.map { case (word, count) => s"$word: $count" }.mkString(" "))
+  def getTopWords(
+      words: mutable.Queue[String],
+      wordCounts: mutable.Map[String, Int],
+      newWords: Seq[String],
+      cloudSize: Int,
+      windowSize: Int
+  ): Seq[(String, Int)] = {
+    newWords.foreach { word =>
+      words.enqueue(word)
+      wordCounts.updateWith(word) {
+        case Some(count) => Some(count + 1)
+        case None        => Some(1)
+      }
+    }
 
-    // to handle SIGPIPE
-    if (System.out.checkError()) {
+    if (windowSize > 0) {
+      while (words.length > windowSize) {
+        val removedWord = words.dequeue()
+        wordCounts.updateWith(removedWord) {
+          case Some(count) if count > 1 => Some(count - 1)
+          case _                        => None
+        }
+        ()
+      }
+    }
+
+    wordCounts.toSeq.sortBy(-_._2).take(cloudSize)
+  }
+
+  def printWordCloud(wordCounts: mutable.Map[String, Int], cloudSize: Int, out: PrintStream = System.out): Unit = {
+    val topWords = wordCounts.toSeq.sortBy(-_._2).take(cloudSize)
+    out.println(topWords.map { case (word, count) => s"$word: $count" }.mkString(" "))
+
+    // Handle SIGPIPE
+    if (out.checkError()) {
       System.exit(1)
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    val (cloudSize, minLength, windowSize) = parseArgs(args)
+  def run(
+      input: Iterator[String],
+      output: PrintStream,
+      config: Config
+  ): Unit = {
     val words = mutable.Queue[String]()
+    val wordCounts = mutable.Map[String, Int]()
 
-    while (true) {
-      val input = StdIn.readLine()
-      if (input == null) sys.exit(0)
-      val validWords = input.split("\\s+").filter(_.length >= minLength)
+    input.foreach { line =>
+      val validWords = line.split("\\s+").filter(_.length >= config.minLength).toSeq
 
-      words.enqueueAll(validWords)
-      if (words.length > windowSize) {
-        val excess = words.length - windowSize
-        for (_ <- 1 to excess) words.dequeue()
-        }
-
-
-      if (words.nonEmpty) {
-        printWordCloud(words, cloudSize)
-        }
+      getTopWords(words, wordCounts, validWords, config.cloudSize, config.windowSize)
+      printWordCloud(wordCounts, config.cloudSize, output)
     }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val config = parseArgs(args)
+    val input = io.Source.stdin.getLines()
+    val output = System.out
+    run(input, output, config)
   }
 }
